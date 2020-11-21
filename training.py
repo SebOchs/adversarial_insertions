b181483478af8e7b1e3f4458b63ad4a705cfe938
SebOchs <ochs.seb@gmail.com>
2020-11-21 14:59:06 +0100

    edit for server

--- training.py
+++ training.py
@@ -5,17 +5,17 @@ from lit_BERT import LitBERT
 checkpoint_callback = ModelCheckpoint(
     monitor="val_macro",
     mode="max",
-    filepath='models/albert_{epoch}-{val_macro:.4f}',
+    filepath='models/bert_{epoch}-{val_macro:.4f}',
     save_top_k=3
 )
 test = LitBERT()
 trainer = pl.Trainer(
-    gpus=1,
-    #num_nodes=1,
-    #distributed_backend='ddp',
+    gpus=2,
+    num_nodes=1,
+    distributed_backend='ddp',
     max_epochs=16,
     checkpoint_callback=checkpoint_callback,
-    accumulate_grad_batches=32
+    accumulate_grad_batches=8
 )
 
 trainer.fit(test)
