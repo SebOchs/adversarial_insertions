988df449295491505d9e3e8dee352d9e2dbd4f9c
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2020-11-21 15:26:11 +0100

    implemented adversarial adjective and adverb attack

--- training.py
+++ training.py
@@ -15,7 +15,7 @@ trainer = pl.Trainer(
     distributed_backend='ddp',
     max_epochs=8,
     checkpoint_callback=checkpoint_callback,
-    accumulate_grad_batches=8
+    accumulate_grad_batches=16
 )
 
 trainer.fit(test)
