6c9fd960b18cc461ed0afa17438783d6d4e1f5e0
Sebastian Ochs <sebastian.ochs@stud.tu-darmstadt.de>
2021-06-04 14:20:21 +0200

    prettyprints for reading examples

--- pretty_displaying.py
+++ pretty_displaying.py
@@ -1,24 +1,15 @@
 import numpy as np
-import random
+import pprint
 
 
 def pretty(path):
     # just a function to display reading examples
     data = np.load(path, allow_pickle=True).item()
-    display = []
+
     for key, value in data.items():
         for i in value:
-            display.append(key + i)
-    # print(random.choice(display))
-    """
-    for i in display:
-        print(i)
-    """
-    """
-    random.shuffle(xd)
-    for i in xd[:10]:
-        print(i)
-    """
+            pprint.pprint(i)
+            print(80*'=')
 
 
-pretty('results/T5/msrpc/reading.npy')
+pretty('results/bert/msrpc/reading.npy')
